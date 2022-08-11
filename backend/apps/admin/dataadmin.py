547f29f74a55aad4e29c8e8a96a00526df1a5cf5
zhangjun <ibmzhangjun@139.com>
2022-08-11 22:27:56 +0800

    syncdb

--- backend/apps/admin/dataadmin.py
+++ backend/apps/admin/dataadmin.py
@@ -23,12 +23,11 @@ from util.log import log as log
 @site.register_admin
 class DataApiApp(admin.AdminApp):
     page_schema = amis.PageSchema(label='Tables', icon='fa fa-tools', sort=1)
-    router_prefix = '/admin'
+    router_prefix = '/data'
 
     def __init__(self, app: "AdminApp"):
         super().__init__(app)
         self.register_admin(DataHome)
-        log.debug('=====================adminstarrt-')
 
 
 class DataHome(admin.PageAdmin):
