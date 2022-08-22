6625eae1cb3a31eacd05b47acea674f8d800912c
zhangjun <ibmzhangjun@139.com>
2022-08-22 11:44:21 +0800

    i18n

--- backend/apps/admin/admin.py
+++ backend/apps/admin/admin.py
@@ -37,7 +37,7 @@ from main import dsconfig, apiengine, dbmeta
 # DataApp
 @site.register_admin
 class DataApp(admin.AdminApp):
-    page_schema = amis.PageSchema(label=_('Data Explore'), icon='fa fa-table', sort=99)
+    page_schema = amis.PageSchema(label=_('Data Explore'), title='Capricornus - '+_('Data Explore'), icon='fa fa-table', sort=99)
     router_prefix = '/data'
     engine = apiengine.async_connect()
 
@@ -63,8 +63,8 @@ class BlankDataPageAdmin(admin.PageAdmin):
 # Data Query Page
 @site.register_admin
 class DataQueryAdmin(admin.PageAdmin):
-    group_schema = amis.PageSchema(label=_('Data Query'), icon='fa fa-user-minus', sort=98)
-    page_schema = PageSchema(label=_('Data Query'), icon='fa fa-user-minus')
+    group_schema = amis.PageSchema(label=_('Data Query'), icon='fa fa-laptop', sort=98)
+    page_schema = PageSchema(label=_('Data Query'), icon='fa fa-laptop')
     # 通过page类属性直接配置页面信息;
     page = Page(title=_('Data Query'), body='')
 
--- backend/core/adminsite.py
+++ backend/core/adminsite.py
@@ -8,16 +8,71 @@
 #  @Author  : Zhang Jun
 #  @Email   : ibmzhangjun@139.com
 #  @Software: Capricornus
+import os
 
+from fastapi import FastAPI
 from fastapi_amis_admin.admin import DocsAdmin, ReDocsAdmin
-
-from core.settings import settings
+from fastapi_amis_admin.amis import AmisAPI, SizeEnum
+from fastapi_amis_admin.amis.components import App, Flex, ActionType, Drawer, Service
+from sqlalchemy.ext.asyncio import AsyncEngine
+from starlette.requests import Request
+from core.settings import settings, Settings
 from fastapi_user_auth.site import AuthAdminSite
-
+from core import i18n as _
 from util.log import log as log
 
-site = AuthAdminSite(settings)
+CORE_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMP_DIR = os.path.join(CORE_DIR, 'templates/app.html')
+
+class CapricornusAdminSite(AuthAdminSite):
+    template_name = TEMP_DIR
+
+    def __init__(self, settings: Settings, fastapi: FastAPI = None, engine: AsyncEngine = None):
+        super().__init__(settings, fastapi, engine)
+        # 取消注册默认管理类
+        self.unregister_admin(DocsAdmin, ReDocsAdmin)
+
+    async def get_page(self, request: Request) -> App:
+        app = await super().get_page(request)
+        app.brandName = settings.site_title
+        app.logo = '/static/images/logo.png'
+        user_auth_app = self.get_admin_or_create(self.UserAuthApp)
+        for item in app.header.items:
+            if item.type == 'dropdown-button' and item.label == f"{request.user.username}":
+                app.header.items.remove(item)
+        app.header = Flex(className="w-full", justify='flex-end', alignItems='flex-end', items=[app.header, {
+            "type": "dropdown-button",
+            "label": f"{request.user.username}",
+            "trigger": "hover",
+            "icon": "fa fa-user",
+            "buttons": [
+                ActionType.Drawer(
+                    label=_('User Profile'),
+                    drawer=Drawer(
+                        title=_('User Profile'),
+                        position="right",
+                        showCloseButton=True,
+                        overlay=False,
+                        closeOnOutside=False,
+                        actions=[],
+                        size=SizeEnum.md,
+                        body=Service(
+                            schemaApi=AmisAPI(
+                                method='post',
+                                url=f"{user_auth_app.router_path}/form/userinfo",
+                                cache=600000,
+                                responseData={'&': '${body}'}
+                            )))),
+                ActionType.Url(
+                    label=_('Sign out'),
+                    url=f"{user_auth_app.router_path}/logout"
+                ),
+            ]
+        }])
+        return app
+
+
+site = CapricornusAdminSite(settings)
 auth = site.auth
 site.UserAuthApp.page_schema.sort = -99
-site.unregister_admin(DocsAdmin, ReDocsAdmin)
 
