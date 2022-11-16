9226a83fa92475f10ba2980ef4907a1ec62e6da4
zhangjun <ibmzhangjun@139.com>
2022-11-16 11:57:13 +0800

    update _sqlmdel add commit

--- backend/docker/bd_build/50_start_h.sh
+++ backend/docker/bd_build/50_start_h.sh
@@ -7,11 +7,11 @@ else
     cp /opt/CapricornusNG/backend/appconfig/hypercorn_default.py /opt/CapricornusNG/backend/appconfig/hypercorn.py
 fi
 
-FIND_DB_FILE="/opt/CapricornusNG/backend/appconfig/hypercorn.py"
+FIND_DB_FILE="/opt/CapricornusNG/backend/utils/crud/sample.db"
 if [ ! -f "$FIND_DB_FILE" ]; then
     echo "sample database exist"
 else
-    cp /opt/CapricornusNG/backend/crud/sample.db /opt/CapricornusNG/backend/data/
+    cp /opt/CapricornusNG/backend/utils/crud/sample.db /opt/CapricornusNG/backend/data/
 fi
 cd /opt/CapricornusNG/backend && \
 nohup /opt/CapricornusNG/backend/venv/bin/hypercorn -c /opt/CapricornusNG/backend/appconfig/hypercorn.py main:app >> /tmp/CapricornusNG.log 2>&1 &
